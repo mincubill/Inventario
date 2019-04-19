8cf8797601bcb6c8bb4b8eb1a629cf4da5c319a7
Ignacio Donoso <naxonicolas1997@gmail.com>
2019-04-19 15:19:41 -0400

    Eliminar y agregar nuevas paginas
    Se eliminaron ciertas paginas que no usaremos como agregar,quitar, ver
    categoria y quitar productos.
    Se agrego la pagina login, y se modifico el nav y ver productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AgregarCategoriaComponent } from './agregar-categoria/agregar-categoria.component';
-import { VerCategoriaComponent } from './ver-categoria/ver-categoria.component';
-import { QuitarCategoriaComponent } from './quitar-categoria/quitar-categoria.component';
 import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
 import { VerProductoComponent } from './ver-producto/ver-producto.component';
-import { QuitarProductoComponent } from './quitar-producto/quitar-producto.component';
 import { NavComponent } from './nav/nav.component';
 import { IngresarStockComponent } from './ingresar-stock/ingresar-stock.component';
 import { SacarStockComponent } from './sacar-stock/sacar-stock.component';
 import { IngresarPrestamoComponent } from './ingresar-prestamo/ingresar-prestamo.component';
 import { IngresarDevolucionComponent } from './ingresar-devolucion/ingresar-devolucion.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AgregarCategoriaComponent,
-    VerCategoriaComponent,
-    QuitarCategoriaComponent,
     AgregarProductoComponent,
     VerProductoComponent,
-    QuitarProductoComponent,
     NavComponent,
     IngresarStockComponent,
     SacarStockComponent,
     IngresarPrestamoComponent,
-    IngresarDevolucionComponent
+    IngresarDevolucionComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AgregarCategoriaComponent } from './agregar-categoria.component';
+import { LoginComponent } from './login.component';
 
-describe('AgregarCategoriaComponent', () => {
-  let component: AgregarCategoriaComponent;
-  let fixture: ComponentFixture<AgregarCategoriaComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AgregarCategoriaComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AgregarCategoriaComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
