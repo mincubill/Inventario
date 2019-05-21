fbf5fb585e0a53d34fa758dd29a6d82cdc8a7a92
yisus <higlord@gmail>
2019-05-21 17:23:12 -0400

    avance en webservice y correcion de algunas cosas en la base de datos

---
+++ server/dao/productDao.js
@@ -0,0 +1,84 @@
+const con = require("../dao/connection.js");
+
+const createProduct = function(product){
+    let query = "select FN_CREATE_PRODUCT(?,?,?,?,?)";
+    return new Promise((resolve, reject) => {
+        con.query(query, [product.name,product.description,product.stock,product.price,product.store], (error, result, fields) => {
+            if(error){
+                console.log(error);
+                reject(error);
+                return;
+            }
+            let keys = Object.keys(result[0]);
+            resolve(result[0][keys[0]]); 
+        });
+    });
+};
+
+const updateProduct = function(product){
+    let query = "select FN_UPDATE_PRODUCT(?,?,?,?,?,?)";
+    return new Promise((resolve, reject) => {
+        con.query(query, [product.id,product.name,product.description,product.stock,product.price,product.store], (error, result, fields) => {
+            if(error){
+                console.log(error);
+                reject(error);
+                return;
+            }
+            let keys = Object.keys(result[0]);
+            resolve(result[0][keys[0]]); 
+        });
+    });
+};
+
+const updateStatusProduct = function(product){
+    let query = "select FN_UPDATE_STATUS_PRODUCT(?,?)";
+    return new Promise((resolve, reject) => {
+        con.query(query, [product.id,product.status], (error, result, fields) => {
+            if(error){
+                console.log(error);
+                reject(error);
+                return;
+            }
+            let keys = Object.keys(result[0]);
+            resolve(result[0][keys[0]]); 
+        });
+    });
+};
+
+const updateStockProduct = function(product){
+    let query = "select FN_UPDATE_STOCK_PRODUCT(?,?)";
+    return new Promise((resolve, reject) => {
+        con.query(query, [product.id,product.stock], (error, result, fields) => {
+            if(error){
+                console.log(error);
+                reject(error);
+                return;
+            }
+            let keys = Object.keys(result[0]);
+            resolve(result[0][keys[0]]); 
+        });
+    });
+};
+
+const getProducts = function(){
+    let query = "SELECT * FROM PRODUCT";
+    return new Promise((resolve, reject) => {
+        con.query(query, (error, result, fields) => {
+            if(error){
+                console.log(error);
+                reject(error);
+                return;
+            }
+            let keys = Object.keys(result[0]);
+            resolve(result[0][keys[0]]); 
+        });
+    });
+};
+
+module.exports.createProduct = createProduct;
+module.exports.updateProduct = updateProduct;
+module.exports.updateStatusProduct = updateStatusProduct;
+module.exports.updateStockProduct = updateStockProduct;
+module.exports.getProducts = getProducts;
+
+
