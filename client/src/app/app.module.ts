683bd441854d1fe7e14ad8527fd0f9675a8b6714
mincubill <mincubill@gmail.com>
2019-05-26 17:28:19 -0400

    Agregado mesaje de confirmacion para creacion y eliminacion de productos

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+// import { MatButtonModule, MatDialogModule } from '@angular/material';
 
-
+import { CustomMaterialModule } from './custom-material/custom-material.module';
 import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
 import { VerProductoComponent } from './ver-producto/ver-producto.component';
 import { NavComponent } from './nav/nav.component';
@@ -17,6 +18,8 @@ import { VerPrestamoComponent } from './ver-prestamo/ver-prestamo.component';
 import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
 import { RetiroProductosComponent } from './retiro-productos/retiro-productos.component';
 import { RegistroComponent } from './registro/registro.component';
+import { PopupComponent } from './popup/popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -32,14 +35,18 @@ import { RegistroComponent } from './registro/registro.component';
     VerPrestamoComponent,
     PerfilUsuarioComponent,
     RetiroProductosComponent,
-    RegistroComponent
+    RegistroComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
   providers: [],
+  entryComponents: [PopupComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/popup/popup.component.ts
@@ -0,0 +1,37 @@
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.css']
+})
+export class PopupComponent implements OnInit {
+  title: string;
+  message: string;
+
+  constructor(public dialogRef: MatDialogRef<PopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: PopupComponent) { 
+      this.title = data.title;
+      this.message = data.message;
+    }
+
+  ngOnInit() {
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+
+  onDismiss(): void {
+    this.dialogRef.close(false);
+  }
+
+}
+
+export class PopupModel {
+
+  constructor(public title: string, public message: string){
+
+  }
+}
