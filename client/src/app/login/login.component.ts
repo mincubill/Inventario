d6083cf07cff41c253f67774722dd5aa0ee5c2de
Rigo <ri.manquel@alumnos.duoc.cl>
2019-06-04 09:34:44 -0400

    Login con mensaje de error
    arreglos varios y implementacion de mensaje de error

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, NgControlStatus, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { PopupComponent, PopupModel } from '../popup/popup.component';
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-login',
@@ -11,33 +12,33 @@ import { PopupComponent, PopupModel } from '../popup/popup.component';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  
+  validador:boolean;
   LoginForm: FormGroup;
   constructor( private formBuilder: FormBuilder, private http : HttpClient, private router : Router ) { 
     this.LoginForm = this.formBuilder.group({
       username: [''],
-      password: [''],
+      password: [''],      
     });
   }
 
-  ngOnInit() {
+  ngOnInit() {    
   }
 
   Login() {    
     this.http.post( 'http://127.0.0.1:3000/login', {      
       username: this.LoginForm.controls.username.value,
       password: this.LoginForm.controls.password.value      
-    }).subscribe( ( res : any ) => {
-      console.log(res);
+    }).subscribe( ( res : any ) => {      
       if(  res != null && res != undefined ) {        
         localStorage.setItem( 'rut', res.RUT );
         localStorage.setItem( 'name', res.NAME );
-        localStorage.setItem( 'type', res.TYPE ); 
+        localStorage.setItem( 'type', res.TYPE );         
         // this.router.navigate(['']);
         location.href = '';
       }
-      else {
-        alert( 'Usuario o contraseña incorrectos' );
+      else {                
+        this.validador = true;        
       }
     },
     (error) => {
